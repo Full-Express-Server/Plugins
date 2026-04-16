d86b45754581e34628ed8ec4656b45afd4b6ce1c
Flagen <tf430297services@gmail.com>
2026-04-16 11:44:30 -0500

    Update FES.example.js

--- FES.example.js
+++ FES.example.js
@@ -3,8 +3,9 @@ module.exports = {
     description: "This is the example of a plugin.",
     enabled: true,
     name: "example",
-    run: (payload) => {
-        console.log("This log is coming from the plugin example.js! You can replace this code and create your own plugin.");
+    run: (exposed, payload) => {
+        if (payload?.firstStart) exposed.log("This log is coming from the plugin example.js! You can replace this code and create your own plugin.", { type: "info" });
+        else exposed.log("This log is coming from the plugin example.js! You can replace this code and create your own plugin. This log is from a different payload, so it will run every time the plugins function is ran, not just on server start.");
     },
     version: 1
 };
